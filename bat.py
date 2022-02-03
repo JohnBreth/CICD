c7b15e0ed96809cd714d42245d52ff1b6be0883f
johnbreth <jbreth1@gmail.com>
2022-02-03 17:58:15 -0500

    Simple SSH connection script to run "show ip int brief"

---
+++ bat.py
@@ -0,0 +1,12 @@
+from netmiko import ConnectHandler
+
+iosv_l2_s1 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.1.233',
+    'username': 'jbc',
+    'password': 'cisco',
+}
+
+net_connect = ConnectHandler(**iosv_l2_s1)
+output = net_connect.send_command('show ip int brief')
+print(output)
