e31a3c3bc3b5cc07109b8a8c6eb881aefe959077
johnbreth <jbreth1@gmail.com>
2022-02-04 17:21:02 -0500

    Updated test creds

--- bat.py
+++ bat.py
@@ -3,7 +3,7 @@ from netmiko import ConnectHandler
 iosv_l2_s1 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.1.233',
-    'username': 'jbc',
+    'username': 'test',
     'password': 'cisco',
 }
 
