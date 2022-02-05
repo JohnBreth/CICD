28f20041aa5ed65b02572985886c423a1036f09b
johnbreth <jbreth1@gmail.com>
2022-02-04 19:03:06 -0500

    Updated with custom container image

--- bat.py
+++ bat.py
@@ -4,7 +4,7 @@ iosv_l2_s1 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.1.233',
     'username': 'test',
-    'password': 'cisco'
+    'password': 'cisco',
 }
 
 net_connect = ConnectHandler(**iosv_l2_s1)
