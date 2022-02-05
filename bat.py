4be8bc4bfcd0d5b73897401d17251f678c632695
johnbreth <jbreth1@gmail.com>
2022-02-04 19:16:20 -0500

    Updated with custom container image

--- bat.py
+++ bat.py
@@ -1,12 +1,12 @@
 from netmiko import ConnectHandler
 
 iosv_l2_s1 = {
-    'device_type': 'cisco_ios',
-    'ip': '192.168.1.233',
-    'username': 'test',
-    'password': 'cisco',
+    "device_type": "cisco_ios",
+    "ip": "192.168.1.233",
+    "username": "test",
+    "password": "cisco",
 }
 
 net_connect = ConnectHandler(**iosv_l2_s1)
-output = net_connect.send_command('show ip int brief')
+output = net_connect.send_command("show ip int brief")
 print(output)
