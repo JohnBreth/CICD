cc07194705d719e634327ae7aa105ddf3c5b6ad1
johnbreth <jbreth1@gmail.com>
2022-02-06 18:00:47 -0500

    Updated IP

--- bat.py
+++ bat.py
@@ -6,7 +6,7 @@ password = os.environ["PASSWORD"]
 
 iosv_l2_s1 = {
     "device_type": "cisco_ios",
-    "ip": "192.168.1.233",
+    "ip": "192.168.1.234",
     "username": "test",
     "password": password,
 }
