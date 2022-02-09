7ee1ee8bea78aab3df926fc6fcffa1be204b2fe6
johnbreth <jbreth1@gmail.com>
2022-02-09 11:44:39 -0500

    Adding Batfish Config Check

---
+++ batfishcheck.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+"""Script used to test the network with batfish"""
+
+from pybatfish.client.commands import *
+from pybatfish.question import load_questions
+from pybatfish.client.asserts import (
+    assert_no_duplicate_router_ids,
+    assert_no_incompatible_bgp_sessions,
+    assert_no_incompatible_ospf_sessions,
+    assert_no_unestablished_bgp_sessions,
+    assert_no_undefined_references,
+)
+from rich.console import Console
+
+
+console = Console(color_system="truecolor")
+
+
+def test_duplicate_rtr_ids(snap):
+    """Testing for duplicate router IDs"""
+    console.print(
+        ":white_exclamation_mark: [bold yellow]Testing for duplicate router IDs[/bold yellow] :white_exclamation_mark:"
+    )
+    assert_no_duplicate_router_ids(
+        snapshot=snap,
+        protocols={"ospf", "bgp"},
+    )
+    console.print(
+        ":green_heart: [bold green]No duplicate router IDs found[/bold green] :green_heart:"
+    )
+
+
+def test_bgp_compatibility(snap):
+    """Testing for incompatible BGP sessions"""
+    console.print(
+        ":white_exclamation_mark: [bold yellow]Testing for incompatible BGP sessions[/bold yellow] :white_exclamation_mark:"
+    )
+    assert_no_incompatible_bgp_sessions(
+        snapshot=snap,
+    )
+    console.print(
+        ":green_heart: [bold green]All BGP sessions compatible![/bold green] :green_heart:"
+    )
+
+
+def test_ospf_compatibility(snap):
+    """Testing for incompatible OSPF sessions"""
+    console.print(
+        ":white_exclamation_mark: [bold yellow]Testing for incompatible OSPF sessions[/bold yellow] :white_exclamation_mark:"
+    )
+    assert_no_incompatible_ospf_sessions(
+        snapshot=snap,
+    )
+    console.print(
+        ":green_heart: [bold green]All OSPF sessions compatible![/bold green] :green_heart:"
+    )
+
+
+def test_bgp_unestablished(snap):
+    """Testing for BGP sessions that are not established"""
+    console.print(
+        ":white_exclamation_mark: [bold yellow]Testing for unestablished BGP sessions[/bold yellow] :white_exclamation_mark:"
+    )
+    assert_no_unestablished_bgp_sessions(
+        snapshot=snap,
+    )
+    console.print(
+        ":green_heart: [bold green]All BGP sessions are established![/bold green] :green_heart:"
+    )
+
+
+def test_undefined_references(snap):
+    """Testing for any undefined references"""
+    console.print(
+        ":white_exclamation_mark: [bold yellow]Testing for undefined references[/bold yellow] :white_exclamation_mark:"
+    )
+    assert_no_undefined_references(
+        snapshot=snap,
+    )
+    console.print(
+        ":green_heart: [bold green]No undefined refences found![/bold green] :green_heart:"
+    )
+
+
+def main():
+    """init all the things"""
+    NETWORK_NAME = "JBC_NET"
+    SNAPSHOT_NAME = "snapshot00"
+    SNAPSHOT_DIR = "./snapshots"
+    bf_session.host = "192.168.24.134"
+    bf_set_network(NETWORK_NAME)
+    init_snap = bf_init_snapshot(SNAPSHOT_DIR, name=SNAPSHOT_NAME, overwrite=True)
+    load_questions()
+    test_duplicate_rtr_ids(init_snap)
+    test_bgp_compatibility(init_snap)
+    test_ospf_compatibility(init_snap)
+    test_bgp_unestablished(init_snap)
+    test_undefined_references(init_snap)
+
+
+if __name__ == "__main__":
+    main()
+
